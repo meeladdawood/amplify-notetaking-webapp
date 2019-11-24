f43598f4062eed479520e7c6a69db2d53f102cd7
Meelad Dawood <mdawood@sandiego.edu>
2019-11-23 18:24:00 -0800

    made a mutatuing graphql call

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
+import { API, graphqlOperation } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
+import { createNote, deleteNote} from "./graphql/mutations";
 
 function App() {
-  const [notes, setNotes] = useState([{
-    id: 1,
-    noteData: 'sample note'
-  }]);
+  const [newNote, setNewNote] = useState('')
+  const [notes, setNotes] = useState([]);
 
-  const l1 = 'flex flex-column items-center justify-center pa3 bg-washed-red'
   const renderedNotes = notes.map((note) => {
     return (
       <div
         key={note.id}
         className='flex items-center'>
         <li className="pa1 f3">
-          {note.noteData}
+          {note.note}
         </li>
         <button className="bg-transparent bn f4">
           <span> &times;</span>
@@ -23,14 +22,29 @@ function App() {
     )
   })
 
+  const handleAddNote = async (event) => {
+      event.preventDefault();
+      const input = {
+        note: newNote
+      }
+
+      const result = await API.graphql(graphqlOperation(createNote, {input}));
+      const newlyAddedNote = result.data.createNote;
+      const newListOfNotes = [newlyAddedNote, ...notes];
+      setNotes(newListOfNotes)
+      setNewNote('')
+  }
+
   return (
-    <div className={l1}>
+    <div className='flex flex-column items-center justify-center pa3 bg-washed-red'>
       <h1 className="code f2-1">Note Taker</h1>
-      <form className="mb3">
+      <form className="mb3" onSubmit={handleAddNote}>
         <input
           type="text"
           className="pa2 f4"
-          placeholder='Write your note' />
+          placeholder='Write your note' 
+          onChange={(e)=> setNewNote(e.target.value)}
+          value={newNote}/>
         <button
           className="pa2 f4"
           type='submit'>
