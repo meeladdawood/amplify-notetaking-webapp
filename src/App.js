e9955fd3141c4a513744f6116902a0085918b584
Meelad Dawood <mdawood@sandiego.edu>
2019-11-23 20:02:02 -0800

    lint fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { API, graphqlOperation } from "aws-amplify";
 import { withAuthenticator } from "aws-amplify-react";
 
-import { createNote, deleteNote} from "./graphql/mutations";
+import { createNote, deleteNote } from "./graphql/mutations";
 import { listNotes } from './graphql/queries';
 
 function App() {
@@ -21,23 +21,23 @@ function App() {
 
   // ALL EVENT HANDLING BELOW
   const handleAddNote = async (event) => {
-      event.preventDefault();
-      const input = { note: newNote }
+    event.preventDefault();
+    const input = { note: newNote }
 
-      const result = await API.graphql(graphqlOperation(createNote, {input}));
-      const newlyAddedNote = result.data.createNote;
-      const newListOfNotes = [newlyAddedNote, ...notes];
-      setNotes(newListOfNotes)
-      setNewNote('')
+    const result = await API.graphql(graphqlOperation(createNote, { input }));
+    const newlyAddedNote = result.data.createNote;
+    const newListOfNotes = [newlyAddedNote, ...notes];
+    setNotes(newListOfNotes)
+    setNewNote('')
   }
 
   const handleDeleteNote = async (noteId) => {
     const input = { id: noteId }
 
-    const result = await API.graphql(graphqlOperation(deleteNote, {input}))
+    const result = await API.graphql(graphqlOperation(deleteNote, { input }))
     const deletedNoteId = result.data.deleteNote.id;
 
-    const updatedNotes = notes.filter((note)=> note.id !== deletedNoteId)
+    const updatedNotes = notes.filter(note => note.id !== deletedNoteId)
     setNotes(updatedNotes);
   }
 
@@ -50,9 +50,9 @@ function App() {
         <li className="pa1 f3">
           {note.note}
         </li>
-        <button 
-        className="bg-transparent bn f4"
-        onClick={()=>handleDeleteNote(note.id)}>
+        <button
+          className="bg-transparent bn f4"
+          onClick={() => handleDeleteNote(note.id)}>
           <span> &times;</span>
         </button>
       </div>
@@ -66,9 +66,9 @@ function App() {
         <input
           type="text"
           className="pa2 f4"
-          placeholder='Write your note' 
-          onChange={(e)=> setNewNote(e.target.value)}
-          value={newNote}/>
+          placeholder='Write your note'
+          onChange={e => setNewNote(e.target.value)}
+          value={newNote} />
         <button
           className="pa2 f4"
           type='submit'>
